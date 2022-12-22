78bb8e812e9b46ff413cd86b61bf995515605341
Emma Gale <emmajgale@hotmail.com>
2022-12-22 17:03:47 +0000

    2.1 defining a component called course which takes any number of parts

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,60 +1,63 @@
-const Header = (props) => {
-	return <h1>{props.course}</h1>;
-};
-
-const Content = (props) => {
-	return (
-		<div>
-			<Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-			<Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-			<Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-		</div>
-	);
-};
-
-const Total = (props) => {
-	return (
-		<p>
-			Number of exercises{" "}
-			{props.parts[0].exercises +
-				props.parts[1].exercises +
-				props.parts[2].exercises}
-		</p>
-	);
-};
-
-const Part = (props) => {
-	return (
-		<p>
-			{props.part} {props.exercises}
-		</p>
-	);
-};
-
-const App = () => {
-	const course = {
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-			},
-		],
-	};
-
+const Course = ({ course }) => {
 	return (
 		<div>
 			<Header course={course.name} />
 			<Content parts={course.parts} />
-			<Total parts={course.parts} />
+			{/* <Total
+				sum={
+					course.parts[0].exercises +
+					course.parts[1].exercises +
+					course.parts[2].exercises
+				}
+			/> */}
+		</div>
+	);
+};
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => (
+	<div>
+		{parts.map((part) => (
+			<Part key={part.id} part={part} />
+		))}
+	</div>
+);
+
+const Part = ({ part }) => (
+	<p>
+		{part.name} {part.exercises}
+	</p>
+);
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const App = () => {
+	const course = {
+		id: 1,
+		name: "Half Stack application development",
+
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+				id: 1,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+				id: 2,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+				id: 3,
+			},
+		],
+	};
+	return (
+		<div>
+			<Course course={course} />
 		</div>
 	);
 };
