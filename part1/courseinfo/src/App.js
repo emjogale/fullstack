864bc9c50790cb9cb9d79ec002e709011c590128
Emma Gale <emmajgale@hotmail.com>
2022-12-23 16:35:23 +0000

    2.3 refactor by moving the reduce function for the sum into the total component rather than the course component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,11 +4,7 @@ const Course = ({ course }) => {
 		<div>
 			<Header course={course.name} />
 			<Content parts={course.parts} />
-			<Total
-				sum={course.parts
-					.map((part) => part.exercises)
-					.reduce((accum, curr) => (accum += curr), 0)}
-			/>
+			<Total parts={course.parts} />
 		</div>
 	);
 };
@@ -29,7 +25,11 @@ const Part = ({ part }) => (
 	</p>
 );
 
-const Total = ({ sum }) => <h3>total of {sum} exercises</h3>;
+const Total = ({ parts }) => (
+	<h3>
+		total of {parts.reduce((sum, part) => (sum += part.exercises), 0)} exercises
+	</h3>
+);
 
 const App = () => {
 	const course = {
@@ -50,6 +50,11 @@ const App = () => {
 			{
 				name: "State of a component",
 				exercises: 14,
+				id: 3,
+			},
+			{
+				name: "Redux",
+				exercises: 11,
 				id: 3,
 			},
 		],
