a0c38d04ce68d2a1f85ce1246140544e59604716
Emma Gale <emmajgale@hotmail.com>
2022-12-24 16:03:01 +0000

    2.4 extend application to allow for an arbitrary number of courses

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,15 +1,23 @@
-const Course = ({ course }) => {
-	console.log(course.parts);
+const Curriculum = ({ curriculum }) => {
+	return <h1>{curriculum}</h1>;
+};
+const Course = ({ courses }) => {
+	console.log(courses);
+
 	return (
 		<div>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
+			{courses.map((course) => (
+				<div>
+					<Header key={course.id} course={course.name} />
+					<Content key={course.parts.id} parts={course.parts} />
+					<Total parts={course.parts} />
+				</div>
+			))}
 		</div>
 	);
 };
 
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h2>{course}</h2>;
 
 const Content = ({ parts }) => (
 	<div>
@@ -32,38 +40,61 @@ const Total = ({ parts }) => (
 );
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: "Half Stack application development",
+	const curriculum = "Web development curriculum";
+	const courses = [
+		{
+			id: 1,
+			name: "Half Stack application development",
 
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-				id: 1,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-				id: 2,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-				id: 3,
-			},
-			{
-				name: "Redux",
-				exercises: 11,
-				id: 3,
-			},
-		],
-	};
+			parts: [
+				{
+					name: "Fundamentals of React",
+					exercises: 10,
+					id: 1,
+				},
+				{
+					name: "Using props to pass data",
+					exercises: 7,
+					id: 2,
+				},
+				{
+					name: "State of a component",
+					exercises: 14,
+					id: 3,
+				},
+				{
+					name: "Redux",
+					exercises: 11,
+					id: 4,
+				},
+			],
+		},
+		{
+			id: 2,
+			name: "Node.js",
+
+			parts: [
+				{
+					name: "Routing",
+					exercises: 3,
+					id: 1,
+				},
+				{
+					name: "Middlewares",
+					exercises: 7,
+					id: 2,
+				},
+			],
+		},
+	];
 	return (
 		<div>
-			<Course course={course} />
+			<Curriculum curriculum={curriculum} />
+			<Course courses={courses} />
 		</div>
 	);
 };
 
 export default App;
+
+// TODO: refactor to loop through courses
