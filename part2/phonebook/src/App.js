409630685b1a03050b2fb1fb592cf64079d7d413
Emma Gale <emmajgale@hotmail.com>
2023-01-07 11:01:16 +0000

    2.11 create dd.json file and modify application so the initial state of the data is fetched from the server using the axios library

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
@@ -9,6 +10,16 @@ const App = () => {
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [filter, setFilter] = useState("");
+
+	const hook = () => {
+		axios.get("http://localhost:3001/persons").then((response) => {
+			setPersons(response.data);
+		});
+	};
+
+	useEffect(hook, []);
+
+	console.log("render", persons.length, "persons");
 
 	const addPerson = (event) => {
 		event.preventDefault();
