8c563185826f99f1c5c7326f4a0b8fac71bb24ca
Emma Gale <emmajgale@hotmail.com>
2023-03-17 09:45:58 +0000

    change the backend so that deleting entries is reflected in the database and move error handling to a new error handler middleware

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
+const { request, response } = require("express");
 
 const requestLogger = (request, response, next) => {
 	console.log("Method:", request.method);
@@ -30,36 +31,11 @@ morgan.token("json", function (req, res) {
 	return JSON.stringify(req.body);
 });
 
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
+let persons = [];
 
 const generateId = () => {
 	return Math.floor(Math.random() * 1000000);
 };
-
-app.get("/", (req, res) => {
-	res.send("<h1>Hello World!</h1>");
-});
 
 app.get("/api/persons", (request, response) => {
 	Person.find({}).then((persons) => {
@@ -74,16 +50,24 @@ app.get("/api/info", (request, response) => {
 	);
 });
 
-app.get("/api/persons/:id", (request, response) => {
-	Person.findById(request.params.id).then((person) => {
-		response.json(person);
-	});
+app.get("/api/persons/:id", (request, response, next) => {
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((person) => person.id !== id);
-	response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -109,7 +93,21 @@ app.post("/api/persons", (request, response) => {
 	});
 });
 
+// handler for requests with unknown endpoint
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.log(error.message);
+	if (error.name === "CastError") {
+		return response.status(400).send({
+			error: "malformatted id",
+		});
+	}
+	next(error);
+};
+
+// handler of requests with result errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
