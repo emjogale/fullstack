cb113f27cc4d16efa7b447e0270c852ea71a4176
Emma Gale <emmajgale@hotmail.com>
2023-04-27 21:06:41 +0100

    add HTTP POST request to add new users with username, password and name and GET POST request to see the deatails of all users

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,9 +1,12 @@
 const supertest = require("supertest");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 
+const User = require("../models/user");
 const Blog = require("../models/blog");
 
 beforeEach(async () => {
@@ -122,6 +125,39 @@ describe("updating bloglikes", () => {
 		await api.put(`/api/blogs/${blogToUpdate.id}`).expect(200);
 
 		expect(updatedBlog.likes - blogToUpdate.likes).toBe(1);
+	});
+});
+
+describe("when there is initially one user at db", () => {
+	beforeEach(async () => {
+		await User.deleteMany({});
+
+		const passwordHash = await bcrypt.hash("sekret", 10);
+		const user = new User({ username: "root", passwordHash });
+
+		await user.save();
+	});
+
+	test("creation succeeds with a fresh username", async () => {
+		const usersAtStart = await helper.usersInDb();
+
+		const newUser = {
+			username: "emma",
+			name: "Emma Gale",
+			password: "salainen",
+		};
+
+		await api
+			.post("/api/users")
+			.send(newUser)
+			.expect(201)
+			.expect("Content-Type", /application\/json/);
+
+		const usersAtEnd = await helper.usersInDb();
+		expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+		const usernames = usersAtEnd.map((u) => u.username);
+		expect(usernames).toContain(newUser.username);
 	});
 });
 
