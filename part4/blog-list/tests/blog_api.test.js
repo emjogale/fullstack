ed329be671fe7fd75688de4e3fe9e6c23b416b67
Emma Gale <emmajgale@hotmail.com>
2023-04-30 09:32:29 +0100

    4.16 require new userto have username and password, both min 3 characters plus tests

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -117,11 +117,9 @@ describe("updating bloglikes", () => {
 	test("adding a vote increases its likes by 1", async () => {
 		const blogsAtStart = await helper.blogsInDb();
 		const blogToUpdate = blogsAtStart[1];
-		console.log("likes", blogToUpdate.likes);
 
 		const updatedBlog = { ...blogToUpdate, likes: blogToUpdate.likes + 1 };
 
-		console.log("likes now", updatedBlog.likes);
 		await api.put(`/api/blogs/${blogToUpdate.id}`).expect(200);
 
 		expect(updatedBlog.likes - blogToUpdate.likes).toBe(1);
@@ -143,8 +141,8 @@ describe("when there is initially one user at db", () => {
 
 		const newUser = {
 			username: "emma",
-			name: "Emma Gale",
-			password: "salainen",
+			name: "Mrs Clod",
+			password: "secret",
 		};
 
 		await api
@@ -158,6 +156,71 @@ describe("when there is initially one user at db", () => {
 
 		const usernames = usersAtEnd.map((u) => u.username);
 		expect(usernames).toContain(newUser.username);
+	});
+
+	test("creation fails with a duplicate username", async () => {
+		const usersAtStart = await helper.usersInDb();
+
+		const newUser = {
+			username: "root",
+			name: "Mrs Clod",
+			password: "anothersecret",
+		};
+
+		const result = await api
+			.post("/api/users")
+			.send(newUser)
+			.expect(400)
+			.expect("Content-Type", /application\/json/);
+
+		expect(result.body.error).toContain("expected `username` to be unique");
+
+		const usersAtEnd = await helper.usersInDb();
+		expect(usersAtEnd).toHaveLength(usersAtStart.length);
+	});
+
+	test("creation fails with a username less than 3 characters long", async () => {
+		const usersAtStart = await helper.usersInDb();
+
+		const newUser = {
+			username: "cl",
+			name: "Mrs Clod",
+			password: "anothersecret",
+		};
+
+		const result = await api
+			.post("/api/users")
+			.send(newUser)
+			.expect(400)
+			.expect("Content-Type", /application\/json/);
+
+		expect(result.body.error).toContain(
+			`shorter than the minimum allowed length (3)`
+		);
+
+		const usersAtEnd = await helper.usersInDb();
+		expect(usersAtEnd).toHaveLength(usersAtStart.length);
+	});
+
+	test("creation fails with a password less than 3 characters long", async () => {
+		const usersAtStart = await helper.usersInDb();
+
+		const newUser = {
+			username: "clod",
+			name: "Mrs Clod",
+			password: "s",
+		};
+
+		const result = await api
+			.post("/api/users")
+			.send(newUser)
+			.expect(400)
+			.expect("Content-Type", /application\/json/);
+
+		expect(result.body.error).toContain("invalid password");
+
+		const usersAtEnd = await helper.usersInDb();
+		expect(usersAtEnd).toHaveLength(usersAtStart.length);
 	});
 });
 
--- part4/blog-list/tests/test_helper.js
+++ part4/blog-list/tests/test_helper.js
@@ -61,7 +61,6 @@ const usersInDb = async () => {
 	const users = await User.find({});
 	return users.map((u) => u.toJSON());
 };
-
 module.exports = {
 	initialBlogs,
 	blogsInDb,
