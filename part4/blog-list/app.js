6d5b10aee40f1ae8de62cea94d1ecc4638e560e6
Emma Gale <emmajgale@hotmail.com>
2023-06-10 13:57:22 +0100

    require express async errors

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,6 +1,5 @@
 const config = require("./utils/config");
 const express = require("express");
-require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const logger = require("./utils/logger");
