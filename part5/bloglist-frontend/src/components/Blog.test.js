99bef61c6c4c1dd665b6cc5b8759aaf532c1d27d
Emma Gale <emmajgale@hotmail.com>
2023-07-06 18:01:05 +0100

    5.15 test if the like button is clicked twice the event handler received as props is called twice

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,22 +1,38 @@
-import Reactimport from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import Blog from './Blog'
+import Reactimport from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Blog from './Blog';
 
-test('renders title and author but not url or likes by default', () => {
-  const blog = {
-    title: 'Looking into the displaying of a blog',
-    author: 'Mrs Bloggy',
-    user: 'user',
-    url: 'test url',
-    likes: 5,
-  }
-  render(<Blog blog={blog} />)
-  const element = screen.getByText(
-    'Looking into the displaying of a blog Mrs Bloggy'
-  )
+describe('<Blog />', () => {
+  test('renders title and author but not url or likes by default', () => {
+    const blog = {
+      title: 'Looking into the displaying of a blog',
+      author: 'Mrs Bloggy',
+      user: 'user',
+      url: 'test url',
+      likes: 5,
+    };
+    render(<Blog blog={blog} />);
+    const element = screen.getByText(
+      'Looking into the displaying of a blog Mrs Bloggy'
+    );
+  });
+  test('if like button is clicked twice then 2 more likes are added to the blog', async () => {
+    const blog = {
+      title: 'Really popular blog',
+      author: 'Mr Bloggy',
+      user: 'user',
+      url: 'test url',
+      likes: 75,
+    };
+    const mockHandler = jest.fn();
+    render(<Blog blog={blog} addLike={mockHandler} />);
+    const user = userEvent.setup();
+    const button = screen.getByText('like');
+    await user.click(button);
+    await user.click(button);
 
-  screen.debug(element)
-  expect(element).toBeDefined()
-})
+    expect(mockHandler.mock.calls).toHaveLength(2);
+  });
+});
