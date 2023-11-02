1e49558fc5c08700a9e329793520fd84d6e33537
Emma Gale <emmajgale@hotmail.com>
2023-11-02 20:17:29 +0000

    6.14 fetch anecdotes from the json-server backend

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,8 +2,17 @@ import AnecdoteForm from './components/AndecdoteForm';
 import AnecdoteList from './components/AndecdoteList';
 import Filter from './components/Filter';
 import Notification from './components/Notification';
+import anecdoteService from './services/anecdotes';
+import { setAnecdotes } from './reducers/anecdoteReducer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then((notes) => dispatch(setAnecdotes(notes)));
+  }, []);
+
   return (
     <div>
       <Notification />
